00ee574f1dea7d288418d3e74e80e62333bee1f3
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-03-27 15:28:19 +0000

    - Prepped for 6.4.0 Final

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'marketplace';
-$app['version'] = '1.4.20';
+$app['version'] = '1.4.30';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-marketplace.spec
+++ packaging/app-marketplace.spec
@@ -1,7 +1,7 @@
 
 Name: app-marketplace
 Epoch: 1
-Version: 1.4.20
+Version: 1.4.30
 Release: 1%{dist}
 Summary: Marketplace
 License: Proprietary
