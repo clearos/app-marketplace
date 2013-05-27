5a1b3b66b78a00cc87e8be5eecdd4bc2145d5570
Ben Chambers <bchambers@clearfoundation.com>
2013-05-27 18:45:16 +0000

    - Added images/icons - Removed hard-coded text on intro page - Cleaned up intro page

--- views/wizard_intro.php
+++ views/wizard_intro.php
@@ -26,38 +26,32 @@ $this->lang->load('marketplace');
 // TODO: translate
 // TODO: move style elements to theme
 
-//if ($is_professional)
-//    $image = 'pro-marketplace.png';
-//else
- //   $image = 'community-marketplace.png';
-
 $banner = "<img style='float: right; padding-left: 20px;' src='" . clearos_app_htdocs('marketplace') . "/marketplace.png' alt=''>";
-$banner .= "<h2 style='font-size: 1.8em; color: #909090; width: 687px;'>Getting Started</h2>";
-$banner .= "<p style='font-size: 1.2em; line-height: 20px;'>Congratulations!  You are now ready to install apps and integrated cloud services through the ClearCenter Marketplace.  You will find a large selection of both free and paid apps that can be installed in a few short steps.";
+$banner .= "<h2 style='font-size: 1.8em; color: #909090; width: 687px;'>" . lang('marketplace_getting_started') . "</h2>";
+$banner .= "<p style='font-size: 1.2em; line-height: 20px;'>" . lang('marketplace_wizard_congrats') . "</p>";
 
 $banner .= "<div class='marketplace-wizard-option-container'>";
 $banner .= "  <div class='mode marketplace-wizard-options' id='mode1'>";
-$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>Option #1 - Install by Function</div>";
-$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/market_default.png' alt=''>";
-$banner .= "    <div style='text-align: left; padding-top: 10px;'>If you are new to ClearOS or prefer to configure the system by specific tasks (eg. prevent users from accssing certain websites, share files between users etc.), select this option.</div>";
+$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>" . lang('marketplace_install_by_function') . "</div>";
+$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/marketplace_feature_50x50.png' alt=''>";
+$banner .= "    <div style='text-align: left; padding-top: 10px;'>" . lang('marketplace_install_by_function_description') . "</div>";
 $banner .= "  </div>";
 $banner .= "  <div  class='mode marketplace-wizard-options' id='mode2'>";
-$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>Option #2 - Install by Category</div>";
-$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/market_default.png' alt=''>";
-$banner .= "    <div style='text-align: left; padding-top: 10px;'>Displays groups of apps based on a category classification system used in the main menu.  If you are familiar with basic networking/server terms and familiar with the ClearOS Marketplace, select this option.</div>";
+$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>" . lang('marketplace_install_by_category') . "</div>";
+$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/marketplace_category_50x50.png' alt=''>";
+$banner .= "    <div style='text-align: left; padding-top: 10px;'>" . lang('marketplace_install_by_category_description') . "</div>";
 $banner .= "  </div>";
 $banner .= "  <div class='mode marketplace-wizard-options' id='mode3'>";
-$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>Option #3 - Quick Select File</div>";
-$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/market_default.png' alt=''>";
-$banner .= "    <div style='text-align: left; padding-top: 10px;'>ClearCenter and the ClearFoundation community have a wide selection of pre-configured templates that will quickly get you up and running.</div>";
+$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>" . lang('marketplace_install_by_qsf') . "</div>";
+$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/marketplace_qsf_50x50.png' alt=''>";
+$banner .= "    <div style='text-align: left; padding-top: 10px;'>" . lang('marketplace_install_by_qsf_description') . "</div>";
 $banner .= "  </div>";
 $banner .= "  <div class='mode marketplace-wizard-options' id='mode4'>";
-$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>Option #4 - Skip Wizard</div>";
-$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/market_default.png' alt=''>";
-$banner .= "    <div style='text-align: left; padding-top: 10px;'>Skip the Marketplace wizard.  You can install apps at any time by navigating to the Marketplace.</div>";
+$banner .= "    <div style='font-size: 1.4em; font-weight: bold;'>" . lang('marketplace_skip_wizard') . "</div>";
+$banner .= "<img style='float: right; padding: 10px 0px 5px 10px;' src='" . clearos_app_htdocs('marketplace') . "/marketplace_exit_50x50.png' alt=''>";
+$banner .= "    <div style='text-align: left; padding-top: 10px;'>" . lang('marketplace_skip_wizard_description') . "</div>";
 $banner .= "  </div>";
 $banner .= "</div>";
-//$banner .= "<p align='center'><img src='" . clearos_app_htdocs('marketplace') . "/$image' alt='Marketplace'></p>";
 
 ///////////////////////////////////////////////////////////////////////////////
 // Form
