e09eacf85e451106c079975e5cab0a426518b5d8
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-11-06 20:31:52 +0000

    - Tagged for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'marketplace';
-$app['version'] = '1.5.6';
+$app['version'] = '1.5.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-marketplace.spec
+++ packaging/app-marketplace.spec
@@ -1,7 +1,7 @@
 
 Name: app-marketplace
 Epoch: 1
-Version: 1.5.6
+Version: 1.5.10
 Release: 1%{dist}
 Summary: Marketplace
 License: Proprietary
