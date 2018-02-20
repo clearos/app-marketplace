4eb61546be0db8363ef9e5b118ecb3ba2cf54644
Benjamin Chambers <benjamin@egloo.ca>
2018-02-19 20:51:47 -0500

    Added new rules [Tracker #19121]

--- deploy/cart_rules.php
+++ deploy/cart_rules.php
@@ -20,7 +20,9 @@ $rules = array(
             'app-owncloud-business',
             'app-owncloud-extension',
             'app-samba-directory',
-            'app-wpad'
+            'app-wpad',
+            'app-kopano-basic',
+            'app-kopano-pro',
         ),
         'app-samba-directory' => array(
             'app-google-apps',
@@ -36,35 +38,49 @@ $rules = array(
             'app-owncloud-extension',
             'app-password-policies',
             'app-active-directory',
-            'app-wpad'
+            'app-wpad',
+            'app-kopano-basic',
+            'app-kopano-pro',
         ),
         'app-zarafa-community' => array(
             'app-imap',
             'app-samba-directory',
             'app-zarafa-small-business',
             'app-zarafa-professional',
-            'app-kopano-basic'
+            'app-kopano-basic',
+            'app-kopano-pro',
         ),
         'app-zarafa-small-business' => array(
             'app-imap',
             'app-samba-directory',
             'app-zarafa-community',
             'app-zarafa-professional',
-            'app-kopano-basic'
+            'app-kopano-basic',
+            'app-kopano-pro',
         ),
         'app-zarafa-professional' => array(
             'app-imap',
             'app-samba-directory',
             'app-zarafa-community',
             'app-zarafa-small-business',
-            'app-kopano-basic'
+            'app-kopano-basic',
+            'app-kopano-pro',
         ),
         'app-kopano-basic' => array(
             'app-imap',
             'app-samba-directory',
             'app-zarafa-community',
             'app-zarafa-small-business',
-            'app-zarafa-professional'
+            'app-zarafa-professional',
+            'app-kopano-pro',
+        ),
+        'app-kopano-pro' => array(
+            'app-imap',
+            'app-samba-directory',
+            'app-zarafa-community',
+            'app-zarafa-small-business',
+            'app-zarafa-professional',
+            'app-kopano-basic',
         ),
         'app-dnsthingy' => array(
             'app-web-proxy',
@@ -75,6 +91,12 @@ $rules = array(
             'app-web-proxy',
             'app-content-filter',
             'app-dnsthingy',
+        ),
+        'app-clearglass-community' => array(
+            'app-clearglass-enterprise',
+        ),
+        'app-clearglass-enterprise' => array(
+            'app-clearglass-community',
         ),
     ),
     'requires' => array(
@@ -104,6 +126,23 @@ $rules = array(
         'app-owncloud-business-10-users' => 'app-owncloud-business',
         'app-owncloud-business-20-users' => 'app-owncloud-business',
         'app-owncloud-business-50-users' => 'app-owncloud-business',
-        'app-owncloud-business-100-users' => 'app-owncloud-business'
+        'app-owncloud-business-100-users' => 'app-owncloud-business',
+        'app-kopano-basic-5-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-10-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-20-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-25-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-50-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-100-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-250-users' => 'app-zarafa-small-business',
+        'app-kopano-basic-500-users' => 'app-zarafa-small-business',
+        'app-kopano-webmeetings' => 'app-kopano-basic',
+        'app-kopano-webmeetings-5-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-10-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-20-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-25-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-50-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-100-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-250-users' => 'app-kopano-basic',
+        'app-kopano-webmeetings-500-users' => 'app-kopano-basic',
     )
 );
